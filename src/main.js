ec8183ac6fe893f5f5c3b381558725bf32f889ef
enoque <eu.enoqueribeiro@gmail.com>
2025-05-20 00:49:08 -0300

    entrega da atividade

---
+++ src/main.js
@@ -0,0 +1,28 @@
+const listaAlunos = [
+    {
+        nome: "Lucas",
+        nota: 7.5
+    },
+    {
+        nome: "Mariana",
+        nota: 6.0
+    },
+    {
+        nome: "João",
+        nota: 9.0
+    },
+    {
+        nome: "Ana Clara",
+        nota: 10.0
+    },
+    {
+        nome: "Pedro",
+        nota: 5.0
+    }
+];
+
+const alunosAprovados = listaAlunos.filter((item)=>{
+    return item.nota >= 6;
+})
+
+console.log(alunosAprovados);
