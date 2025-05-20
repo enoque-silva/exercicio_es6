db3d719cebc9aa28928b0b2af18f354f154e18cf
enoque <eu.enoqueribeiro@gmail.com>
2025-05-20 00:58:15 -0300

    entrega da atividade

--- src/main.js
+++ src/main.js
@@ -1,27 +1,12 @@
 const listaAlunos = [
-    {
-        nome: "Lucas",
-        nota: 7.5
-    },
-    {
-        nome: "Mariana",
-        nota: 6.0
-    },
-    {
-        nome: "João",
-        nota: 9.0
-    },
-    {
-        nome: "Ana Clara",
-        nota: 10.0
-    },
-    {
-        nome: "Pedro",
-        nota: 5.0
-    }
+    { nome: "Lucas", nota: 7.5 },
+    { nome: "Mariana", nota: 6.0 },
+    { nome: "João", nota: 9.0 },
+    { nome: "Ana Clara", nota: 10.0 },
+    { nome: "Pedro", nota: 5.0 }
 ];
 
-const alunosAprovados = listaAlunos.filter((aluno)=>{
+const alunosAprovados = listaAlunos.filter((aluno) => {
     return aluno.nota >= 6;
 })
 
